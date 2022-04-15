9aa7c009f957d70ea0a7aa0fb12a164331b1cd89
Cristian Castillo de Leon <62943663+Crisca06@users.noreply.github.com>
2022-04-15 17:12:25 +0200

    Ejercicio 4 hecho
    Co-Authored-By: AidaAcha <48960424+AidaAcha@users.noreply.github.com>

---
+++ Practica2.1/ejercicio4.cc
@@ -0,0 +1,71 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <iostream>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char** argv){
+    struct addrinfo hints;
+    struct addrinfo *result;
+    memset(&hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_flags    = AI_PASSIVE; //Devolver 0.0.0.0
+    hints.ai_family   = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM;
+
+    int rc = getaddrinfo(argv[1], argv[2], &hints, &result);
+    int sd = socket(result->ai_family, result->ai_socktype, 0);
+
+    //Si rc devuelve algo distinto de 0 ha habido un error
+    if(rc != 0) {
+        std::cerr << "[getaddrinfo]: " << gai_strerror(rc) << "\n";
+        return -1;
+    }
+
+    if(sd == -1){
+        std::cerr << "ERROR al crear el socket.\n";
+		return -1;
+    }
+
+    bind(sd, (struct sockaddr *) result->ai_addr, result->ai_addrlen);
+    listen(sd, 5);
+
+	freeaddrinfo(result);
+
+    while (1) {
+        char host[NI_MAXHOST];
+        char serv[NI_MAXSERV];
+        struct sockaddr_in cliente;
+        socklen_t cliente_len = sizeof(struct sockaddr_in);
+        int cliente_sd = accept(sd, (struct sockaddr *) &cliente, &cliente_len);
+
+        getnameinfo((struct sockaddr *) &cliente, cliente_len, host, 
+                    NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST|NI_NUMERICSERV);
+        
+        std::cout << "Conexion desde " << host <<  ":" << serv << "\n";
+
+        int i = 0; ssize_t c; bool connected = true;
+
+        while(connected){
+            char buffer[80]; 
+
+            do {
+                c = recv(cliente_sd, &buffer[i], 1, 0);
+                if(c < 1) connected = false;
+            } while ( c >= 0 && i < 79 && buffer[i++] != '\n');
+            
+            if(connected){
+                buffer[i] = '\0';	
+                send(cliente_sd, buffer, i, 0);
+                i = 0;
+            } else 
+                std::cout << "Conexion terminada. \n";
+        }
+        close(cliente_sd);
+    }
+
+    close(sd);
+    return 0;
+}
