a29ee699905831bd5c5f6922a5af15cfe12b1b83
AidaAcha <aidaacha@ucm.es>
2022-05-02 13:23:53 +0200

    cambios ejercicio4
    Co-Authored-By: Cristian Castillo de Leon <62943663+Crisca06@users.noreply.github.com>

--- Practica2.2/Ejercicio 4/Socket.cc
+++ Practica2.2/Ejercicio 4/Socket.cc
@@ -5,28 +5,31 @@
 
 Socket::Socket(const char * address, const char * port):sd(-1)
 {
-    
-    struct addrinfo h;
-    struct addrinfo*  r;
+    //Construir un socket de tipo AF_INET y SOCK_DGRAM usando getaddrinfo.
+    //Con el resultado inicializar los miembros sd, sa y sa_len de la clase
 
-    memset((void *) &h, 0, sizeof(struct addrinfo));
+    struct addinfo h; 
+    struct addinfo* r; 
+
+    memset((void *) &h, 0, sizeof(struct addinfo));
 
     //UDP
     h.ai_family = AF_INET;
     h.ai_socktype = SOCK_DGRAM;
 
-    int rc = getaddrinfo(address, port, &h, r);
-    if (rc != 0)
-	fprintf(stderr, "ERROR: [getaddrinfo]: " << gai_strerror(rc) << "\n";
+    int rec = getaddrinfo(address, port, &h, &r);
+    if(rec != 0)
+        fprintf(stderr, "ERROR EN: [getaddrinfo]: " << 
+        gai_strerror(rec) << "\n");
+    
+    sd = socket(res->ai_family, res->ai_socktype, 0);
+    if(sd == -1)
+        std::cerr << "ERROR EN: [socket] no se creo \n";
 
-    sd = socke r->ai_family r->ai_socktype, 0);
-    if (sd == -1)
-	std::cerr << "ERROR: [socket] no creado\n";
+    sa = *res->ai_addr;
+    sa_len = res->ai_addrlen;
 
-    sa = r->ai_addr;
-    sa_len  r->ai_addrlen;
-
-    freeaddrinf r);
+    freeaddrinfo(res);
 }
 
 Socket::Socket(struct sockaddr* _sa, socklen_t _sa_len) : sd(-1), sa(*_sa), sa_len(_sa_len) {
@@ -58,49 +61,42 @@ int Socket::recv(Serializable &obj, Socket * &sock)
     return 0;
 }
 
-int Socket::recv(Serializable &obj) 
-{
-    Socket * s = 0;
-
-    return recv(obj, s);
-}
-
 int Socket::send(Serializable& obj, const Socket& sock)
 {
-    obj.to_bin();
+    //Serializar el objeto
+
+    obj.to_bin()
+
+    //Enviar el objeto binario a sock usando el socket sd
 
     ssize_t bytes = sendto(sock.sd, obj.data(), obj.size(), 0, &sock, sa, sock.sa_len);
-    if (bytes < 1) {
-	std::cerr << "ERROR: fallo al enviar mensaje\n";
-	return -1;
+    if(bytes < 1){
+        std::cerr << "ERROR: fallo en el envio de mensajes \n";
+        return -1;
     }
     return 0;
 }
 
-int Socket::bind()
+bool operator== (const Socket &s1, const Socket &s2)
 {
-    return ::bind(sd, (const struct sockaddr *) &sa, sa_len);
-}
-
-bool Socket::operator== (const Socket &s1, const Socket &s2)
-{
-    struct sockaddr_in* s1_in = (struct sockaddr_in *) &(s1.sa);
-    struct sockaddr_in* s2_in = (struct sockaddr_in *) &(s2.sa);
+    struct sockaddr_in* s1in = (struct sockaddr_in *) & (s1.sa);
+    struct sockaddr_in* s2in = (struct sockaddr_in *) & (s2.sa);
 
     //Comparar los campos sin_family, sin_addr.s_addr y sin_port
     //de la estructura sockaddr_in de los Sockets s1 y s2
     //Retornar false si alguno difiere
-    if (s1.sa.sa_family != s2.sa.sa_family || s1_in->sin_addr.s_addr != s2_in->sin_addr.s_addr ||
-	s1_in->sin_port != s2_in->sin_port)
-	return false;
+
+    if(s1.sa.sa_family != s2.sa.sa_family || s1in->sin_addr.s_addr != s2in->sin_addr.s_addr ||
+        s1in->sin_port != s2in->sin_port)
+        return false;
     else return true;
 };
 
-bool Socket::operator!= (const Socket &sock1, const Socket &sock2) {
-    return !(sock1 == sock2);
+bool operator!= (const Socket &s1, const Socket &s2) {
+    return !(s1 == s2);
 };
 
-std::ostream& Socket::operator<<(std::ostream& ent, const Socket& s)
+std::ostream& operator<<(std::ostream& os, const Socket& s)
 {
     char host[NI_MAXHOST];
     char serv[NI_MAXSERV];
@@ -108,7 +104,8 @@ std::ostream& Socket::operator<<(std::ostream& ent, const Socket& s)
     getnameinfo((struct sockaddr *) &(s.sa), s.sa_len, host, NI_MAXHOST, serv,
                 NI_MAXSERV, NI_NUMERICHOST);
 
-    ent << "Dir ip: "<< host << ", Puerto: " << serv;
+    os << host << ":" << serv;
 
-    return ent;
+    return os;
 };
+
